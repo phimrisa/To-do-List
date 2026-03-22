96575027ac58ccaaaeb5dfd37c6fe71e9d7968eb
phimrisa <phimrisa@gmail.com>
2026-03-22 23:28:53 +0700

    Update TitleBar.java

--- To-do List Project/src/TitleBar.java
+++ To-do List Project/src/TitleBar.java
@@ -5,13 +5,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TitleBar extends JPanel{
-    TitleBar()
-    {
-        this.setPreferredSize(new Dimension(400,80));
-        JLabel titleText = new JLabel("To-do List");
-        titleText.setPreferredSize(new Dimension(200,60));
-        titleText.setFont(new Font("Sans-serif",Font.PLAIN, 20));
-        titleText.setHorizontalAlignment(JLabel.CENTER);
-        this.add(titleText);
+    TitleBar() {
+        this.setPreferredSize(new Dimension(400,80)); // ขนาดของ Title Bar
+        JLabel titleText = new JLabel("To-do List"); // ข้อความของ Title Bar
+        titleText.setPreferredSize(new Dimension(200, 60)); // ขนาดตัวอักษร Title Bar
+        titleText.setFont(new Font("Sans-serif",Font.PLAIN, 20)); // ฟอนต์ Title Bar
+        titleText.setHorizontalAlignment(JLabel.CENTER); // ทำให้ Title Bar อยู่ตรงกลาง
+        this.add(titleText); // เพิ่มข้อความไปที่ Title Bar
     }
 }
