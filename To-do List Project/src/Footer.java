a2dbc6db67cf876bc249d2b7e97925b871f73da9
phimrisa <phimrisa@gmail.com>
2026-03-22 23:38:48 +0700

    Update Footer.java

--- To-do List Project/src/Footer.java
+++ To-do List Project/src/Footer.java
@@ -6,21 +6,19 @@ class Footer extends JPanel {
     JButton addTask;
     JButton clearAll;
 
-    Border emptyBorder = BorderFactory.createEmptyBorder();
-
     Footer() {
-        this.setPreferredSize(new Dimension(400, 60));
+        this.setPreferredSize(new Dimension(400, 60)); // ขนาดพื้นที่ของ Footer
 
-        addTask = new JButton("Add Task");
-        addTask.setFont(new Font("Sans-serif", Font.PLAIN, 15));
-        addTask.setVerticalAlignment(JButton.BOTTOM);
-        this.add(addTask);
+        addTask = new JButton("Add Task"); // ปุ่ม Add Task
+        addTask.setFont(new Font("Sans-serif", Font.PLAIN, 15)); // ฟอนต์ของปุ่ม Add Task
+        addTask.setVerticalAlignment(JButton.BOTTOM); // ทำให้ปุ่ม Add Task อยู่ด้านล่าง
+        this.add(addTask); // เพิ่มไปที่ Footer
 
-        this.add(Box.createHorizontalStrut(20));
+        this.add(Box.createHorizontalStrut(20)); // ระยะห่างระหว่างปุ่ม Add Task กับ Clear Finished Tasks
 
-        clearAll = new JButton("Clear finished tasks");
-        clearAll.setFont(new Font("Sans-serif", Font.PLAIN, 15));
-        this.add(clearAll);
+        clearAll = new JButton("Clear Finished Tasks"); // ปุ่ม Clear Finished Tasks
+        clearAll.setFont(new Font("Sans-serif", Font.PLAIN, 15)); // ฟอนต์ของปุ่ม Clear Finished Tasks
+        this.add(clearAll); // เพิ่มไปที่ Footer
     }
 
     public JButton getNewTask() {
